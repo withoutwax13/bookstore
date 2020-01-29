6c2fa4c6138b78159a317d3684975d912f2e86a8
user <johnpatrickyusoresvalera@gmail.com>
2020-01-30 01:04:49 +0800

    pages.test.py modification

--- pages/tests.py
+++ pages/tests.py
@@ -1,12 +1,23 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 class HomePageTests(SimpleTestCase):
 
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+    
     def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_homepage_template_name(self):
+        self.assertTemplateUsed(self.response, 'home.html')
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_resolve_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
